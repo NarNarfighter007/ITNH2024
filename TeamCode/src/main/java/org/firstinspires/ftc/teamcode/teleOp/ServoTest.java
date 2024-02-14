ea0e150bc4fe93632ad8ef850e04044adc84261d
NarNarfighter007 <shajpaulme@gmail.com>
2024-02-13 19:39:49 -0500

    new outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/ServoTest.java
@@ -2,31 +2,62 @@ package org.firstinspires.ftc.teamcode.teleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class ServoTest extends OpMode {
-    Servo fourbarServo, dropServo, boxServo, boxRotServo, intake;
+    Servo fourbarServo, leftDropServo, rightDropServo, boxServo, boxRotServo, emergencyOutake;
+    CRServo transferServo;
+
+    DcMotor intakeMotor;
 
     @Override
     public void init() {
         fourbarServo = hardwareMap.get(Servo.class, "FBS00"); //FBS00 //72
-        dropServo = hardwareMap.get(Servo.class, "PDS02"); //
-        boxServo = hardwareMap.get(Servo.class, "BRS01"); //22
-//        boxRotServo = hardwareMap.get(Servo.class, "TWS03");
-        intake = hardwareMap.get(Servo.class, "FUS14");
+        leftDropServo = hardwareMap.get(Servo.class, "DLS02");
+        rightDropServo = hardwareMap.get(Servo.class, "DRS04");
+        boxServo = hardwareMap.get(Servo.class, "BXS01"); //22
+        boxRotServo = hardwareMap.get(Servo.class, "BRS05");
+        emergencyOutake = hardwareMap.get(Servo.class, "EMS03");
+
+        transferServo = hardwareMap.get(CRServo.class, "TNS10");
+        transferServo.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intakeMotor = hardwareMap.get(DcMotor.class, "INM11");
     }
 
     @Override
     public void loop() {
-        fourbarServo.setPosition(gamepad1.left_stick_y);
-        dropServo.setPosition(gamepad1.right_stick_x);
-        boxServo.setPosition(gamepad1.right_stick_y);
-//        boxRotServo.setPosition(gamepad1.left_stick_y);
-//        intake.setPosition(gamepad1.left_stick_y);
+//        fourbarServo.setPosition(gamepad1.left_stick_y);
+//        leftDropServo.setPosition(gamepad1.right_stick_x);
+//        boxServo.setPosition(gamepad1.right_stick_y);
+//
+//        boxRotServo.setPosition(gamepad2.left_stick_y);
+//        emergencyOutake.setPosition(gamepad2.right_stick_y);
+//        rightDropServo.setPosition(gamepad2.right_stick_x);
+        if(gamepad1.right_bumper){
+            intakeMotor.setPower(0.8);
+            transferServo.setPower(1);
+        } else if (gamepad1.left_bumper){
+            transferServo.setPower(-1);
+            intakeMotor.setPower(-0.8);
+        } else {
+            transferServo.setPower(0);
+            intakeMotor.setPower(0);
+        }
+
+
+
+
         telemetry.addData("fourbar", fourbarServo.getPosition());
-        telemetry.addData("drop", dropServo.getPosition());
+        telemetry.addData("Leftdrop", leftDropServo.getPosition());
         telemetry.addData("box", boxServo.getPosition());
+        telemetry.addData("rightDrop", rightDropServo.getPosition());
+        telemetry.addData("box", boxServo.getPosition());
+        telemetry.addData("boxRot", boxRotServo.getPosition());
 //        telemetry.addData("box rot", boxRotServo.getPosition());
 //        telemetry.addData("intake", intake.getPosition());
 
